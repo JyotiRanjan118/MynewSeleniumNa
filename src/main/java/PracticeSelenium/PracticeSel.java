6996a1b1f89e378a9af6298fd2f555bb15665a33
Jyoti Ranjan Nath <jyoti.ranjan20@ibm.com>
2020-10-11 23:34:40 +0530

    Selenium edited

--- src/main/java/PracticeSelenium/PracticeSel.java
+++ src/main/java/PracticeSelenium/PracticeSel.java
@@ -1,20 +1,40 @@
 package PracticeSelenium;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class PracticeSel {
 
 	public static void main(String[] args)  {
-		
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
 		
-		driver.get("https://www.facebook.com/");
+		driver.get("https://app.hubspot.com/login");
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		
+		WebElement email=driver.findElement(By.id("username"));
+		WebElement password=driver.findElement(By.id("password"));
+		WebElement loginBtn=driver.findElement(By.id("loginBtn"));
+		
+		Actions action=new Actions(driver);
+		action.sendKeys(email,"jrnbabu118@gmail.com");
+		action.sendKeys(password,"jrnBABU@118");
+		action.click(loginBtn).build().perform();
+		
+		String headerValue=driver.findElement(By.xpath("//h1[contains(@class,'private-header')]")).getText();
+		System.out.println(headerValue);
 		
 		
 	}
-
+	
 }
